91790d8aeecc5e9c5b5eb08116a42336ba8767d8
Olga <ti-dzhy@yandex.ru>
2024-05-28 21:22:54 +0300

    Добавили решение 3 задачи

---
+++ task3/Program.cs
@@ -0,0 +1,16 @@
+﻿//Задача 3
+//Задайте произвольный массив. Выведете его элементы, начиная с конца. Использовать рекурсию, не использовать циклы.
+
+void ShowArray (int [] array, int checksize)
+{
+    if (checksize > array.Length-1)
+        return;
+        
+    if (checksize < array.Length)
+    {
+        ShowArray(array, checksize+1); 
+        Console.Write (" " + array[checksize]);
+    }
+}
+int [] array = new int [] {1, 2, 3, 4, 5, 6};
+ShowArray (array, 0);
